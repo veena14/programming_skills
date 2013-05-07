60237e8608e5799452d1b3d4379cb8e3b7c6d5df
Manoj <manu.tele@gmail.com>
2013-05-07 18:54:01 +0530

    added strstr function

--- string_funcs.c
+++ string_funcs.c
@@ -1,21 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-int main()
-{
-    int option;
-
-    printf(" \nEnter option \n\tTo reverse_words_instring - 1\n");
-    scanf("%d",&option);
-    switch(option)
-    {
-        case 1:
-            reverse_word_instring();
-            break;
-        
-        default : break;
-	}
-    return 0;
-}
 
 int reverse_word_instring()
 {
@@ -42,8 +26,65 @@ int reverse_word_instring()
 			i--;
 		}
 		len++;
-
 	}
 	printf("\nreverse string is :--%s\n", str);
 	return 0;	
 }
+
+int sub_string()
+{
+    char *str = "SC nod to Kudankulam nuclear plant";
+    char *substr = "nkulu";
+    int sublen = 0, i, found;
+
+    while(*substr)
+    {
+        sublen++;
+        substr++;
+    }    
+  
+    substr = substr - sublen;
+   while(*str)
+    {
+        found = 0;
+        for(i=0;i<sublen; i++)
+        {
+            if(*str == *substr)
+                found++;
+            if(found == sublen)
+            {
+                printf("\nfound\n");
+                return 0;
+            }
+            *str++;
+            *substr++;
+        }
+
+        substr = substr - found;
+        str = str - (sublen -1);
+    }
+     printf("\nsub string not found\n");
+     return 0;
+}
+
+int main()
+{
+    int option;
+
+    printf(" \nEnter option \n\tTo reverse_words_instring - 1\n\t strstr - 2\n");
+    scanf("%d",&option);
+    switch(option)
+    {
+        case 1:
+            reverse_word_instring();
+            break;
+        case 2:
+            sub_string();
+            break;
+        
+        default : break;
+	}
+    return 0;
+}
+
+
